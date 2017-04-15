fa051560978ff37925bdda2d4356fc6cc93c1aae
mata.chauhan <mata.chauhan@LAP0000082>
2017-04-15 17:55:30 +0530

    fix build issue

--- Web/Models/VMFileDelete.cs
+++ Web/Models/VMFileDelete.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using static Web.Helper.Common;
+using Web.Helper;
 
 namespace Web.Models
 {
     public class VMFileDelete
     {
-        public FileType FileType { get; set; }
+        public Common.FileType FileType { get; set; }
         public string FileName { get; set; }
     }
 }
